b0c2e1b3442e661cbd2e94030f829964ccfb3257
COLLEGELAB-02 <COLLEGELAB-02@BJSB.LAB>
2026-05-02 17:32:06 +0800

    Card added properties and used array

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import './App.css'
-import Card from './Card'
+import './App.css';
+import Card from './Card';
 
 function App() {
+  const carditems = [
+    { header: "Chapter 1", title: "Objectives", description: "Objectives of the study", buttontext: "View Scope and Delimitation" },
+    { header: "Chapter 2", title: "Review of Related Literature", description: "Related Systems", buttontext: "Show More" },
+    { header: "Chapter 3", title: "Methodology", description: "Method of Development", buttontext: "See Charts" }
+  ];
 
   return (
     <>
-     <>Hello World</>
-     <Card>
-      
-     </Card>
+      <h1>Hello World</h1>
+      {carditems.map((item, index) => (
+        <Card 
+          key={index}
+          header={ index + ". " + item.header}
+          title={item.title}
+          description={item.description}
+          buttontext={item.buttontext}
+        />
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,16 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import type { CardProp } from './CardProp'
 
-function Card() {
+function Card(cardprop : CardProp ) {
     return(
         <>
         <div className="card">
             <div className="card-header">
-                Featured
+                {cardprop.header}
             </div>
                 <div className="card-body">
-                    <h5 className="card-title">Happy BirthDay!</h5>
-                    <p className="card-text">Just a kid from tondo</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <h5 className="card-title">{cardprop.title}</h5>
+                    <p className="card-text">{cardprop.description}</p>
+                    <a href="#" className="btn btn-primary">{cardprop.buttontext}</a>
                 </div>
             </div>
         </>
