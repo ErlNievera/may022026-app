2c7412ff5a7433c44b335f67c2d3bc9ed26ebca3
COLLEGELAB-02 <COLLEGELAB-02@BJSB.LAB>
2026-05-02 17:48:26 +0800

    hastitle used

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import Card from './Card';
 
 function App() {
   const carditems = [
-    { header: "Chapter 1", title: "Objectives", description: "Objectives of the study", buttontext: "View Scope and Delimitation" },
-    { header: "Chapter 2", title: "Review of Related Literature", description: "Related Systems", buttontext: "Show More" },
-    { header: "Chapter 3", title: "Methodology", description: "Method of Development", buttontext: "See Charts" }
+    { header: "Chapter 1", title: "Objectives", description: "Objectives of the study", buttontext: "View Scope and Delimitation", hastitle: false },
+    { header: "Chapter 2", title: "Review of Related Literature", description: "Related Systems", buttontext: "Show More", hastitle: true },
+    { header: "Chapter 3", title: "Methodology", description: "Method of Development", buttontext: "See Charts", hastitle: true }
   ];
 
   return (
@@ -18,6 +18,7 @@ function App() {
           title={item.title}
           description={item.description}
           buttontext={item.buttontext}
+          hastitle={item.hastitle}
         />
       ))}
     </>
